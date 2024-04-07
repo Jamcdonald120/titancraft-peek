79862886a704eb6640e844870801d3ef20e2c835
Jesse McDonald <jamcdonald120@gmail.com>
2024-04-06 22:08:11 -1000

    purge

--- src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockEntityMixin.java
+++ src/main/java/de/maxhenkel/peek/mixin/ShulkerBoxBlockEntityMixin.java
@@ -35,56 +35,6 @@ public abstract class ShulkerBoxBlockEntityMixin extends RandomizableContainerBl
         super(blockEntityType, blockPos, blockState);
     }
 
-    @Nullable
-    @Override
-    public Packet<ClientGamePacketListener> getUpdatePacket() {
-        if (!Peek.CONFIG.sendShulkerBoxDataToClient.get()) {
-            return super.getUpdatePacket();
-        }
-        return ClientboundBlockEntityDataPacket.create(this);
-    }
-
-    @Override
-    public CompoundTag getUpdateTag(HolderLookup.Provider provider) {
-        if (!Peek.CONFIG.sendShulkerBoxDataToClient.get()) {
-            return super.getUpdateTag(provider);
-        }
-        CompoundTag tag = new CompoundTag();
-        saveAdditional(tag, provider);
-        if (tag.isEmpty()) {
-            // If the tag is empty, save container items again with an empty items list,so that the update packet is actually processed
-            // Empty tags are replaced with null in the ClientboundBlockEntityDataPacket and thus not processed on the client
-            ContainerHelper.saveAllItems(tag, itemStacks, true, provider);
-        }
-
-        return tag;
-    }
-
-    @Override
-    public void setChanged() {
-        super.setChanged();
-        if (!Peek.CONFIG.sendShulkerBoxDataToClient.get()) {
-            return;
-        }
-        if (level == null || level.isClientSide()) {
-            return;
-        }
-        Packet<ClientGamePacketListener> packet = getUpdatePacket();
-        if (!(packet instanceof ClientboundBlockEntityDataPacket dataPacket)) {
-            return;
-        }
-        CompoundTag tag = dataPacket.getTag();
-        if (tag == null) {
-            tag = new CompoundTag();
-        }
-        if (lastData != null && lastData.equals(tag)) {
-            return;
-        }
-
-        PlayerLookup.tracking(this).forEach(p -> p.connection.send(packet));
-        lastData = tag;
-    }
-
     @Shadow
     protected abstract void saveAdditional(CompoundTag compoundTag, HolderLookup.Provider provider);
 
